c9606d9fcbf08691447c8a5d9b6b39798c12e956
로렌 <118709290+fhkwon@users.noreply.github.com>
2023-01-19 21:28:48 +0900

    [level 0] Title: 가까운 수, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120890. 가까운 수/가까운 수.py
+++ 프로그래머스/lv0/120890. 가까운 수/가까운 수.py
@@ -1,7 +1,3 @@
 def solution(array, n):
-    dic_a = {}
-    for i in array:
-        dic_a[i] = abs(i - n) 
-    aa = sorted(dic_a.items(), key = lambda x: (x[1], x[0]))
-    answer = aa[0][0]
+    answer = 0
     return answer
