bdb62566a78c2c5e626157554d751d47bbe5a2d4
fhkwon <118709290+fhkwon@users.noreply.github.com>
2023-01-19 12:25:16 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.py
@@ -0,0 +1,7 @@
+def solution(numbers):
+    answer = 0
+    nums = {"zero": '0', "one": '1', "two": '2', "three": '3', "four":'4', "five":'5', "six":'6', "seven":'7', "eight":'8', "nine":'9'}
+    for i,v in nums.items():
+        numbers = numbers.replace(i,v)
+    answer = int(numbers)
+    return answer
