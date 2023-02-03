2366a43ae74973aa859e5c136c90686bfdb6af16
로렌 <118709290+fhkwon@users.noreply.github.com>
2023-02-03 11:48:55 +0900

    [unrated] Title: 푸드 파이트 대회, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.py
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.py
@@ -1,12 +1,10 @@
 def solution(food):
-    answer = [0]
+    answer = "0"
     del food[0]
     food.reverse()
     for idx, n in enumerate(food):
         if n < 2:
             pass
         else:
-            answer[0:0] = [len(food)-idx]*(n//2)
-            answer[len(answer):len(answer)] = [len(food)-idx]*(n//2)
-    answer = ''.join(str(i) for i in answer)
+            answer = str(len(food)-idx)*(n//2) + answer + str(len(food)-idx)*(n//2)
     return answer
